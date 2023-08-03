7e385f5b511ec1bf906ba9377531b77e26805237
snehalgg <127075837+snehalgg@users.noreply.github.com>
2023-08-03 14:13:02 +0530

    Add files via upload

---
+++ BookShoppingCartMvcUI/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using BookShoppingCartMvcUI.Data;
+global using System.ComponentModel.DataAnnotations;
+global using BookShoppingCartMvcUI.Models;
+global using BookShoppingCartMvcUI.Repositories;
+global using BookShoppingCartMVCUI.Models;
+global using BookShoppingCartMVCUI.Models.DTOs;
